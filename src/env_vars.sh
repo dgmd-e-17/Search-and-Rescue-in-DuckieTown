8322d66d54e9dbbfc4a7b55bc79297ea6bf1ee51
Artemio Mendoza <artexmg@gmail.com>
2024-03-28 12:46:18 -0500

    first update

---
+++ src/env_vars.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Set the environment variable for the server endpoint URL
+export AWS_SERVER_ENDPOINT_URL='https://r5l50m40ne.execute-api.us-east-1.amazonaws.com/prod/robotics_s3_presignedurl'
